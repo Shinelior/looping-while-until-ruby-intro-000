4174f8787dc41872b876f7105dc0b2f7dbac5e34
lior Shine <shinelior@gmail.com>
2019-11-19 10:28:38 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,8 @@
 levitation_force = 6
 def using_until
-    levitation_force+=1
+    
     until levitation_force >= 10
+         levitation_force+=1
         puts 'Wingardium Leviosa'
      
         
