13c686b4bdafcb44fef9dfbc1483de2162feec42
lior Shine <shinelior@gmail.com>
2019-11-19 10:22:30 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,9 +2,9 @@ levitation_force = 6
 def using_until
     levitation_force+= 1
     until levitation_force > 10
-        break
-        if levitation_force== 10
-          puts 'Wingardium Leviosa'
+        puts 'Wingardium Leviosa'
+      else
+          break
       end
     end
     
