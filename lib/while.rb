0e2bc4ce5c618cef9d7af207a952da19ba8cc05b
lior Shine <shinelior@gmail.com>
2019-11-19 10:00:27 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,12 @@
 levitation_force = 6
 def using_while
-  while 
+  while levitation_force  
   levitation_force += 1
   if levitation_force ==10
 	  puts 'Wingardium Leviosa'
 	end
-  
+	
+ end 
 end
 
 
